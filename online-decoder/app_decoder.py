9128951ff5940d27680991b578f88fced925aec0
Michael Haberler <git@mah.priv.at>
2023-08-16 11:25:44 +0200

    cleanups

--- online-decoder/app_decoder.py
+++ online-decoder/app_decoder.py
@@ -2,25 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import os, time
-from datetime import datetime
 from flask import (
     Flask,
     render_template,
-    jsonify,
     redirect,
-    url_for,
     request,
     make_response,
     send_file,
 )
 from werkzeug.utils import secure_filename
 import io
-import zipfile
 import json
 import ndjson
 import geojson
 from TheengsDecoder import decodeBLE
-from TheengsDecoder import getProperties, getAttribute
 import codecs
 import custom
 from flatten_json import flatten
@@ -28,11 +23,6 @@ import arrow
 import gengpx
 from flask_qrcode import QRcode
 from bleads import decode_advertisement
-
-# pip install git+https://github.com/mhaberler/uttlv.git@ltv-option
-
-# import bleads
-import overpy
 from gentp import Teleplot
 import re
 
@@ -250,7 +240,7 @@ def decode(input, options, destfmt, timestamp, debug=False, customDecoder=None):
         buffer = io.BytesIO()
         buffer.write(tp.toJson().encode("utf-8"))
         buffer.seek(0)
-        return ("teleplot.json", buffer)
+        return ("-teleplot.json", buffer)
 
     massaged = traverse_and_modify(
         j, options=options, useless=useless, timestamp=timestamp, debug=debug
@@ -327,33 +317,6 @@ def sensorlogger():
                 return response
 
 
-@app.route("/animation3D", methods=["GET", "POST"])
-def animation3D():
-    if request.method == "GET":
-        return render_template("animation3D.html")
-    if request.method == "POST":
-        options = request.form.getlist("options")
-        destfmt = request.form.getlist("destfmt")
-        timestamp = request.form.getlist("timestamp")
-        files = request.files.getlist("files")
-        for file in files:
-            fn = secure_filename(file.filename)
-            if fn and allowed_file(fn):
-                input = file.stream.read()
-                # input = file.stream._file.getvalue()
-                (ext, output) = decode(
-                    input, options, destfmt, timestamp, customDecoder=custom.Decoder
-                )
-                base, oldext = os.path.splitext(fn)
-                response = make_response(
-                    send_file(
-                        output,
-                        download_name=base + suffix + ext,
-                        as_attachment=True,
-                    )
-                )
-                return response
-
 
 @app.route("/livetrack", methods=["GET", "POST"])
 def livetrack():
@@ -371,133 +334,6 @@ def tracking():
 
     app.logger.info(f"livetrack post {ah=} {request.data=}")
     return {}
-
-
-@app.route("/traj", methods=["POST"])
-def traj():
-    j = json.loads(request.data)
-    f = open("traj-orig.json", "w")
-    f.write(json.dumps(j, indent=4))
-    f.close()
-    f = open("traj-geojson.json", "w")
-    m = massage(j)
-    f.write(geojson.dumps(m, indent=4))
-    f.close()
-    return {}
-
-
-def get_shops(lat, lon, distance=5000):
-    # Initialize the API
-    api = overpy.Overpass()
-    # Define the query
-    query = (
-        f'(node["shop"](around:{distance},{lat},{lon});'
-        f'node["building"="retail"](around:{distance},{lat},{lon});'
-        f'node["building"="supermarket"](around:{distance},{lat},{lon});'
-        f'node["healthcare"="pharmacy"](around:{distance},{lat},{lon});'
-        f");out;"
-    )
-    # Call the API
-    result = api.query(query)
-    return result
-
-
-# https://mateuszwiza.medium.com/plotting-api-results-on-a-map-using-flask-and-leafletjs-2cf2d3cc660b
-# https://github.com/mateuszwiza/mapping-api-results
-# 47.07585724136853 15.431147228565663
-@app.route("/shops", methods=["GET", "POST"])
-def shops():
-    if request.method == "POST":
-        # The code here determines what happens after sumbitting the form
-        # Get shops data from OpenStreetMap
-        shops = get_shops(request.form["lat"], request.form["lon"], distance=50)
-        # app.logger.info(f"found {len(shops.nodes)} shops")
-
-        # Initialize variables
-        id_counter = 0
-        markers = ""
-        for node in shops.nodes:
-            # Create unique ID for each marker
-            idd = "shop" + str(id_counter)
-            id_counter += 1
-            # Check if shops have name and website in OSM
-            try:
-                shop_brand = node.tags["brand"]
-            except:
-                shop_brand = "null"
-            try:
-                shop_website = node.tags["website"]
-            except:
-                shop_website = "null"
-            # Create the marker and its pop-up for each shop
-            markers += "var {idd} = L.marker([{latitude}, {longitude}]);\
-                        {idd}.addTo(map).bindPopup('{brand}<br>{website}');".format(
-                idd=idd,
-                latitude=node.lat,
-                longitude=node.lon,
-                brand=shop_brand,
-                website=shop_website,
-            )
-
-        # Render the page with the map
-        rt = render_template(
-            "results.html",
-            markers=markers,
-            lat=request.form["lat"],
-            lon=request.form["lon"],
-        )
-        return rt
-    else:
-        # Render the input form
-        return render_template("leaflet.html")
-
-
-# @app.route("/upload", methods=["POST"])
-# def upload():
-#     files = request.files.getlist("files")
-#     for file in files:
-#         fn = secure_filename(file.filename)
-#         if fn and allowed_file(fn):
-#             base, ext = os.path.splitext(fn)
-#             file.save(os.path.join(app.config["UPLOAD_FOLDER"], base + suffix + ext))
-#     return redirect("/")  # change to redirect to your own url
-
-
-# upload several files
-# zip them
-# return the zip archvive
-# @app.route("/zip", methods=["POST"])
-# def zip():
-#     files = request.files.getlist("files")
-#     zipped_file = io.BytesIO()
-#     with zipfile.ZipFile(zipped_file, "w") as zipper:
-#         for file in files:
-#             fn = secure_filename(file.filename)
-#             zipper.writestr(fn, file.stream._file.getvalue())
-#     zipped_file.seek(0)
-#     response = make_response(
-#         send_file(zipped_file, download_name="export.zip", as_attachment=True)
-#     )
-#     return response
-
-
-# upload zip archive
-# return list of files in archive
-# @app.route("/postzip", methods=["POST"])
-# def postzip():
-#     file = request.files["data_zip_file"]
-#     file_like_object = file.stream._file
-#     zipfile_ob = zipfile.ZipFile(file_like_object)
-#     file_names = zipfile_ob.namelist()
-#     # Filter names to only include the filetype that you want:
-#     # file_names = [file_name for file_name in file_names if file_name.endswith(".txt")]
-#     # files = [(zipfile_ob.open(name).read(),name) for name in file_names]
-#     return str(file_names)
-
-
-# @app.route("/download")
-# def download():
-#     return render_template("index.html")
 
 
 if __name__ == "__main__":
